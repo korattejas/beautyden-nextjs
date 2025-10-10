3a34bb685809e674a5ea2d48de03b6a12d43fd6a
tejas.korat <tejas.korat@metizsoft.com>
2025-10-10 15:06:22 +0530

    whatsapp script changes

--- src/components/WhatsAppWidget.tsx
+++ src/components/WhatsAppWidget.tsx
@@ -17,7 +17,7 @@ export default function WhatsAppWidget() {
     script.id = scriptId;
     script.type = "text/javascript";
     script.src = "https://d3mkw6s8thqya7.cloudfront.net/integration-plugin.js";
-    script.setAttribute("widget-id", "aaa54e");
+    script.setAttribute("widget-id", "aaa5h8");
     script.async = true;
 
     // Append to body
